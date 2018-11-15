a21152b9adea1acd9a609771a21fc8a7caf79da7
Quinten De Swaef <quintendeswaef@gmail.com>
2018-11-15 10:17:47 +0100

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+const provider = new PrivateKeyProvider('0x7fc1393a94fb36fba8cffe511702a89b17ce8fee30fd66d334e423dac87147e8', "https://kovan.fundrequest.io/");
+
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    default: {
+      network_id: '42',
+      provider: () => provider
+    }
+  }
+};
