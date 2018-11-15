2669c33055f1438edc15ac8063cdd23f47961315
Quinten De Swaef <quintendeswaef@gmail.com>
2018-11-15 10:33:15 +0100

    initial state

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
 const PrivateKeyProvider = require("truffle-privatekey-provider");
-const provider = new PrivateKeyProvider('0x7fc1393a94fb36fba8cffe511702a89b17ce8fee30fd66d334e423dac87147e8', "https://kovan.fundrequest.io/");
+const provider = new PrivateKeyProvider('7fc1393a94fb36fba8cffe511702a89b17ce8fee30fd66d334e423dac87147e8', "https://kovan.fundrequest.io/");
 
 require('dotenv').config();
 
 module.exports = {
   networks: {
-    default: {
+    kovan: {
       network_id: '42',
       provider: () => provider
     }
