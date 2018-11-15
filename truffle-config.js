a4a2e3a7c8df20ffbbca08381d5c00e9e6b753bf
Quinten De Swaef <quintendeswaef@gmail.com>
2018-11-15 10:41:52 +0100

    finalized state

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,9 @@
-const PrivateKeyProvider = require("truffle-privatekey-provider");
-const provider = new PrivateKeyProvider('7fc1393a94fb36fba8cffe511702a89b17ce8fee30fd66d334e423dac87147e8', "https://kovan.fundrequest.io/");
+const ArkaneProvider = require("@arkane-network/truffle-arkane-provider");
+const provider = new ArkaneProvider({
+  apiKey: '',
+  baseUrl: 'https://api-staging.arkane.network',
+  providerUrl: 'https://kovan.fundrequest.io'
+});
 
 require('dotenv').config();
 
